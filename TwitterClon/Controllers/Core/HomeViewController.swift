05f19d5cf75cc4a220e17a4b4b4f7da2ba296acf
grinya37 <112849355+grinya37@users.noreply.github.com>
2023-07-25 00:06:37 +0300

    add_VC_settings_signs

--- TwitterClon/Controllers/Core/HomeViewController.swift
+++ TwitterClon/Controllers/Core/HomeViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private func configureNavigationBar() {
+        let size: CGFloat = 36
+        let logoImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: size, height: size))
+        logoImageView.contentMode = .scaleAspectFill
+        logoImageView.image = UIImage(named: "twitLogo")
+        
+        let middleView = UIView(frame: CGRect(x: 0, y: 0, width: size, height: size))
+        middleView.addSubview(logoImageView)
+        navigationItem.titleView = middleView
+        
+        let profileImage = UIImage(systemName: "person")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: profileImage, style: .plain, target: self, action: #selector(didTapProfile))
+    }
+    
+    @objc private func didTapProfile() {
+        let vc = ProfileViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
     private let timelineTableView: UITableView = {
         let tableView = UITableView()
         tableView.register(TweetTableViewCell.self, forCellReuseIdentifier: TweetTableViewCell.identifier)
@@ -21,6 +40,7 @@ class HomeViewController: UIViewController {
         timelineTableView.delegate = self
         timelineTableView.dataSource = self
     
+        configureNavigationBar()
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -39,7 +59,29 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TweetTableViewCell.identifier, for: indexPath) as? TweetTableViewCell else {
             return UITableViewCell()
         }
+        cell.delegate = self
         return cell
     }
 
+}
+
+extension HomeViewController: TweetTableViewCellDelegate {
+    
+    func tweetTableViewCellDidTapReply() {
+        print("Reply")
+    }
+    
+    func tweetTableViewCellDidTapRetweet() {
+        print("Retweet")
+    }
+    
+    func tweetTableViewCellDidTapLike() {
+        print("Like")
+    }
+    
+    func tweetTableViewCellDidTapShare() {
+        print("share")
+    }
+    
+    
 }
---
+++ TwitterClon/Controllers/General/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  TwitterClon
+//
+//  Created by Николай Гринько on 24.07.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .tertiarySystemBackground
+        navigationItem.title = "Profile"
+    }
+    
+}
