1a098701630c65c1ccf6e0a0e13afc00d5226c31
grinya37 <112849355+grinya37@users.noreply.github.com>
2023-07-24 13:00:31 +0300

    delete_storyboard_main"info"

---
+++ TwitterClon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClon
+//
+//  Created by Николай Гринько on 24.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
